18112bcac1eae69ba38afa9a908ebf32a2cc98b6
SZGN <szgnmhmmd@gmail.com>
2022-04-06 23:23:53 +0300

    /

--- src/bankamatik/Bank.java
+++ src/bankamatik/Bank.java
@@ -1,22 +1,15 @@
 package bankamatik;
 
 import static bankamatik.Bankamatik.bankamtik;
-import static bankamatik.ParaCekme.paraCekme;
-import static bankamatik.ParaYatir.paraYatir;
+
 
 public class Bank {
     public static void main(String[] args) {
 
         bankamtik();
-       // paraCekme();
-       // paraYatir();
+
 
     }
-
-
-
-
-
 
 
 }
